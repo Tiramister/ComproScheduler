55819041bd5b472e336f53d5b517de65c04c8cae
Tiramister <nh111424@gmail.com>
2020-01-29 14:45:01 +0900

    implement main activity and its layout to display data in SQL

--- app/src/main/java/com/example/comproscheduler/MainActivity.kt
+++ app/src/main/java/com/example/comproscheduler/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.comproscheduler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        realm = Realm.getDefaultInstance()
+
+        // set layout and adapter of the recycler view
+        scheduleListView.layoutManager = LinearLayoutManager(this)
+        val schedules = realm.where<Schedule>().findAll()
+        val adapter = ScheduleAdapter(schedules)
+        scheduleListView.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
